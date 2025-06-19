56f7366150c1eb3324bd0c584986a32c07f35677
amin masoudi <aminmasoudi2003@gmail.com>
2025-06-19 23:32:19 +0330

    add Sub bytes and RC to G function

--- encryption.py
+++ encryption.py
@@ -1,6 +1,10 @@
 import os, hashlib
-
+from s_box import SBOX
 class AESEncryption:
+    def __init__(self, key=None):
+        self.__key = self.gen_key(key)
+        self.__round_keys = self.round_key(self.__key)
+        print(self.__round_keys)
     def gen_key(self, base_key:str=None) -> str :
         """
         ## Gen Function for AES Encryption scheme
@@ -19,28 +23,38 @@ class AESEncryption:
         ## Round key function
         in this function we take a key and return 44 round key from the key
         """
-        words = [key[i*8:i*8+8] for i in range(4)] # [w0, w1, w2, w3]
+        words = ["0x" + key[i*8:i*8+8] for i in range(4)] # [w0, w1, w2, w3]
+        rc= 1
         for i in range(4, 44):
             previous = words[i-1]
             # print(previous)
             if i % 4 == 0:
-                previous = self.__g(previous)
+                previous = self.__g(previous, rc=rc)
+                rc << 1
             new = int(words[i-4], base=16) ^ int(previous, base=16)      
             words.append(hex(new))
         return words
         
-    def __g(self, word: str)->str:
+    def __g(self, word: str, rc:int)->str:
         word = word[2:] if word.startswith("0x") else word
         word = word[2:] + word[:2]
         # TODO: sub table
-        # TODO: RCj
-        return word
+        
+        new_bytes = ""
+        for byte_index in range(0, 4, 2):
+            c_1, c_2 = word[byte_index], word[byte_index+1]
+            byte  = SBOX[int(c_1, base=16)][int(c_2, base=16)] 
+            new_bytes += hex(byte)[2:]
+
+        new_bytes = hex(int(new_bytes[:2], base=16) ^ rc) + new_bytes[2:]
+
+        return new_bytes
     
-    def encrypt(self, plaintext, key=None):
+    def encrypt(self, plaintext):
+        ...
         ...
     def decrypt(self, key):...
     
 a = AESEncryption()
-k2 = a.round_key(a.gen_key("amin2323"))
 # k1 = a.round_key(a.gen_key())
-print(k2, sep="\n")
+a.encrypt()
