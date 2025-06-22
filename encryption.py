4ac6eec96ab2aef16c11c6d6309262667ff2a7e5
amin masoudi <aminmasoudi2003@gmail.com>
2025-06-22 23:49:48 +0330

    Add Decryption + some fixes

--- encryption.py
+++ encryption.py
@@ -1,11 +1,14 @@
 import os, hashlib
-from s_box import SBOX
+from s_box import SBOX, INV_S_BOX
+
+
+
 class AESEncryption:
     def __init__(self, key=None):
         self.__key = self.gen_key(key)
         self.__round_keys = self.round_key(self.__key)
 
-    def gen_key(self, base_key:str=None) -> str :
+    def gen_key(self, base_key:str=None) -> bytes :
         """
         ## Gen Function for AES Encryption scheme
         in this function we take an optional base_key as input and then
@@ -13,41 +16,40 @@ class AESEncryption:
         **Notice:**  if base_key is not given then it generates a new random key             
         """
         if base_key is None:
-            key = os.urandom(16).hex()
+            key = os.urandom(16)
         else:
-            key = hashlib.md5(base_key.encode()).hexdigest()
+            key = hashlib.md5(base_key.encode()).digest()
         return key
     
-    def round_key(self, key:str):
+    def round_key(self, key:bytes)->list[bytes]:
         """
         ## Round key function
         in this function we take a key and return 44 round key from the key
         """
-        words = ["0x" + key[i*8:i*8+8] for i in range(4)] # [w0, w1, w2, w3]
+        # words = ["0x" + key[i*8:i*8+8] for i in range(4)] # [w0, w1, w2, w3]
+        words = [key[i*4:i*4+4] for i in range(4)]
         rc= 1
         for i in range(4, 44):
             previous = words[i-1]
             if i % 4 == 0:
                 previous = self.__g(previous, rc=rc)
                 rc << 1
-            new = int(words[i-4], base=16) ^ int(previous, base=16)      
-            words.append(new.to_bytes(4).hex())
+            new = int(words[i-4].hex(), base=16) ^ int(previous.hex(), base=16)      
+            words.append(new.to_bytes(4))
         return words
         
-    def __g(self, word: str, rc:int)->str:
-        word = word[2:] if word.startswith("0x") else word
-        word = word[2:] + word[:2]
-        # TODO: sub table
-        
-        new_bytes = ""
-        for byte_index in range(0, 4, 2):
-            new_bytes += self.__sub_byte(word[byte_index: byte_index+2])
+    def __g(self, word: bytes, rc:int)->bytes:
+        # word = word[2:] if word.startswith("0x") else word
+        word = word[1:] + word[:1]        
+        new_bytes = b""
+        for byte_index in range(0, 4):
+            new_bytes += self.__sub_byte(word[byte_index:byte_index+1])
 
-        new_bytes = (int(new_bytes[:2], base=16) ^ rc).to_bytes().hex() + new_bytes[2:]
+        new_bytes = (new_bytes[0] ^ rc).to_bytes() + new_bytes[2:]
 
         return new_bytes
     
-    def encrypt(self, plaintext:bytes)->str:
+    def encrypt(self, plaintext:bytes)->bytes:
         # padding
         padding_size = 16 - ((len(plaintext)) % 16)
         plaintext = plaintext + (padding_size * padding_size.to_bytes())
@@ -56,13 +58,37 @@ class AESEncryption:
         # encrypt blocks
         blocks = list(map(lambda block: self.encrypt_block(block) ,blocks))
         # join cipher blocks
-        blocks = ''.join(blocks)
+        blocks = b''.join(blocks)
         return blocks
         # encrypt
+    
+    def decrypt(self, cipher:bytes)->bytes:
+        # split
+        blocks = [cipher[i*16:i*16+16] for i in range(len(cipher)//16)]
+        # decrypt
+        blocks = list(map(lambda block: self.decrypt_block(block), blocks))
+        # unpadding
+        # join
+        blocks = b''.join(blocks)
+        return blocks
+    
+    def decrypt_block(self, block:bytes)->bytes:
+        table = [block[4*i: 4*i+4] for i in range(4)]
+        table = self.__add_round_key(table, self.__round_keys[40:])
+        for round in range(10, 0, -1):
+            table = self.__inv_shift_rows(table)
+            table = self.__inv_sub_bytes(table)
+            table = self.__add_round_key(table, self.__round_keys[round*4 - 4: round*4])
+            if round != 1:
+                table = self.__inv_mix_columns(table)
         
-    def encrypt_block(self, block: bytes) -> str: 
+        block = b''.join(table)
+        return block
+    
+    def encrypt_block(self, block: bytes) -> bytes: 
         # create table
-        table = [''.join([hex(block[j])[2:] for j in range(4*i, 4*i+4)]) for i in range(4)]
+        
+        table = [block[4*i: 4*i+4] for i in range(4)]
         table = self.__add_round_key(table, self.__round_keys[:4])
         for round in range(1, 11):
             table = self.__sub_bytes(table)
@@ -70,45 +96,62 @@ class AESEncryption:
             if round != 10:
                 table = self.__mix_columns(table)
             table = self.__add_round_key(table, self.__round_keys[round*4: round*4 + 4])
-        block = ''.join(table)
+        block = b''.join(table)
         return block
 
-    def __sub_bytes(self, table:list[str])->list[str]:
+    def __inv_sub_bytes(self, table:list[bytes])->list[bytes]:
         for column_index in range(4):
-            new_col = ''
+            new_col = b''
             for i in range(4):
-                new_col += self.__sub_byte(table[column_index][i*2: i*2+2])
+                new_col += self.__inv_sub_byte(table[column_index][i:i+1])
+            table[column_index] = new_col
+        return table
+ 
+
+    def __sub_bytes(self, table:list[bytes])->list[bytes]:
+        for column_index in range(4):
+            new_col = b''
+            for i in range(4):
+                new_col += self.__sub_byte(table[column_index][i:i+1])
             table[column_index] = new_col
         return table
 
-    def __sub_byte(self, word:str)->str:
-        if word.startswith('0x'): word = word[2:]
-        c_1, c_2 = word[0], word[1]
-        byte  = SBOX[int(c_1, base=16)][int(c_2, base=16)] 
-        byte  = byte.to_bytes().hex()
+    def __inv_sub_byte(self, word:bytes)->bytes:
+        word = word[0]
+        # c_1, c_2 = word[0], word[1]
+        # byte  = INV_S_BOX[int(c_1, base=16)][int(c_2, base=16)] 
+        byte = INV_S_BOX[word]
+        byte  = byte.to_bytes()
         return byte
 
-    def __add_round_key(self, table:list[str], keys:list[str]) -> list[str]:
+    def __sub_byte(self, word:bytes)->bytes:
+        word = word.hex()
+        c_1, c_2 = word[0], word[1]
+        byte  = SBOX[int(c_1, base=16)][int(c_2, base=16)] 
+        byte  = byte.to_bytes()
+        return byte
+
+    def __add_round_key(self, table:list[bytes], keys:list[bytes]) -> list[bytes]:
         for i in range(4):
-            r = int(table[i], base=16) ^ int(keys[i], base=16)
-            table[i] = r.to_bytes(4).hex()
+            r = int(table[i].hex(), base=16) ^ int(keys[i].hex(), base=16)
+            table[i] = r.to_bytes(4)
         return table
             
         
-    def __shift_rows(self, table:list[str])->list[str]:
+    def __shift_rows(self, table:list[bytes])->list[bytes]:
         new_table = []
         for counter in range(4):
-            string = "".join(
-                [table[(i + counter) % 4][2*i: 2*i+2] for i in range(0, 4)]
+            string = b"".join(
+                [table[(i + counter) % 4][i:i+1] for i in range(0, 4)]
                 )
             new_table.append(string)
         return new_table
     
-    def __inv_shift_rows(self, table:list[str])-> list[str]:
+    def __inv_shift_rows(self, table:list[bytes])-> list[bytes]:
         new_table = []
         for counter in range(4):
-            string = "".join(
-                [table[(i + counter) % 4][2*j: 2*j+2] for j, i in enumerate(range(0, -4, -1))]
+            string = b"".join(
+                [table[(i + counter) % 4][j:j+1] for j, i in enumerate(range(0, -4, -1))]
             )
             new_table.append(string)
         return new_table
@@ -126,28 +169,24 @@ class AESEncryption:
             b >>= 1
         return p
 
-    def __mix_columns(self, table:list[str])->list[str]:
-        table = [[int(c[2*i:2*i+2], base=16) for i in range(4)] for c in table]
+    def __mix_columns(self, table:list[bytes])->list[bytes]:
         new_table = []
         for c in table:
-            col = []
+            col = b""
             for r in range(4):
                 val: int = (
                     self.__gmul(2, c[r]) 
                     ^ self.__gmul(3 , c[(r +1)%4]) 
                     ^ c[(r  + 2)%4] 
                     ^ c[(r+3)%4])
-                
-                col.append(val.to_bytes().hex())
+                col += val.to_bytes()
             new_table.append(col)
-        table = ["".join(c) for c in new_table]
-        return table
+        return new_table
 
-    def __inv_mix_columns(self, table:list[str])->list[str]:
-        table = [[int(c[2*i:2*i+2], base=16) for i in range(4)] for c in table]
+    def __inv_mix_columns(self, table:list[bytes])->list[bytes]:
         new_table = []
         for c in table:
-            col = []
+            col = b""
             for r in range(4):
                 val: int = (
                     self.__gmul(  0xe, c[r]) 
@@ -155,15 +194,19 @@ class AESEncryption:
                     ^ self.__gmul(0xd, c[(r  + 2)%4]) 
                     ^ self.__gmul(0x9, c[(r+3)%4])
                 )
-                col.append(val.to_bytes().hex())
+                col += val.to_bytes()
             new_table.append(col)
-        table = ["".join(c) for c in new_table]
-        return table
+        return new_table
     
-    def decrypt(self, key):...
 
     
-# a = AESEncryption(key="amin2323")
-# print(a.encrypt(b'hello world! 123'))
-# 5315b6eb8d7d26e1dc601f683334933daa02146b5e51cb44984abf095e3c1e3f
+a = AESEncryption(key="amin2323")
+table = [b"0123", b'4567',b'89ab',b'cdef']
+
+# print(a._inv_mix_columns(a._mix_columns(table)))
+# print((c:=a.encrypt(b'hello world! 123')).hex())
+# print(m:=a.decrypt(c))
+
+# b'e6ed625e7e488f29f41f9c8ae50733e9bcc024f8ea3fcdc4165a1c3d99217608'
+
 
