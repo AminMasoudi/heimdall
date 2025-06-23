c8f75523358635b6079067c406ffe3ed44daa4c5
amin masoudi <aminmasoudi2003@gmail.com>
2025-06-23 12:54:29 +0330

    delete AESCipher class

--- utiles.py
+++ utiles.py
@@ -1,43 +1,13 @@
 
-import base64, os
-import hashlib
-from Crypto.Cipher import AES
+import os
 from api_service import APIService, File
+from encryption import AESEncryption
 
-Random = lambda : os.urandom(AES.block_size)
-
-class AESCipher(object):
-
-    def __init__(self, key):
-        self.bs = AES.block_size
-        self.key = hashlib.sha256(key.encode()).digest()
-
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random()
-        cipher = AES.new(self.key)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
-
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return AESCipher._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
-
-def encrypting(file: str)-> bytes:
-    key = str(os.environ.get("KEY"))
-    return AESCipher(key).encrypt(file)
 
 def decrypt(file):
     key = os.environ("KEY")
-    return AESCipher(key).decrypt(file)
+    aes = AESEncryption(key)
+    return aes.decrypt(file)
 
 def finder(identifier:str)->File:
     files = APIService().list()
@@ -50,4 +20,3 @@ def finder(identifier:str)->File:
     if len(matching) == 1:
         return matching[0]
     raise FileNotFoundError("failed to find the file")
-
